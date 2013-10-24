c46da2eef7358db88e1ced92152b0a0b3ff55100
Kamil Kantyka <kmkantyka@gmail.com>
2013-10-24 19:09:16 -0400

    -Customize bootstrap variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinteresting::Application.routes.draw do
+  get "pages/team"
   root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
